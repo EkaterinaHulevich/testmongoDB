ec78b384e7bbc0519e80b100ebaef46121c43b5d
Ekaterina Hulevich <ekaterina_hulevich@epam.com>
2016-09-23 14:27:14 +0300

    mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -6,27 +6,29 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/myproject';
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
-	var collection = db.collection('documents');
-	collection.remove({});
-	collection.insertMany(user);
-    collection.find({}).toArray(function(err, items) {
+	var collection = db.collection('document');
+	collection.remove({},function(err, affected){
 		assert.equal(null, err);
-		assert.equal(3, items.length);
-
-		for (var i = 0; i < items.length; i++) {
-			console.log(items[i]);
-		}
-    });
-	collection.remove({orderID: 1});
-	collection.find({}).sort( { "orderID": -1} ).toArray(function(err, item) {
-		assert.equal(null, err);
-		assert.equal(2, item.length);
-		var jsonfile = require('jsonfile');
-		var file = './output.json'
-		jsonfile.writeFileSync(file, item, {spaces: 2});	
-    });
-	db.close();
-  
+		collection.insertMany(user);
+		collection.find({}).toArray(function(err, items) {
+			assert.equal(null, err);
+			var len = 3, surnam = "Smith";
+			assert.equal(len, items.length);
+			assert.equal(surnam, items[0].surname);
+			for (var i = 0; i < items.length; i++) {
+				console.log(items[i]);
+			}
+			collection.remove({orderID: 1});
+			collection.find({}).sort( { "orderID": -1} ).toArray(function(err, item) {
+				assert.equal(null, err);
+				assert.equal(2, item.length);
+				var jsonfile = require('jsonfile');
+				var file = './output.json'
+				jsonfile.writeFileSync(file, item, {spaces: 2});
+				db.close();				
+			});
+		});
+	});
 });
 
 
